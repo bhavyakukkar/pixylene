c288ec329a399cd2f3e3ed35708fb1e9ac1fb8a4
bhavyakukkar <bhavyakukkar@gmail.com>
2023-11-18 18:57:30 +0530

    add project.rs

---
+++ src/project.rs
@@ -0,0 +1,12 @@
+use crate::layer::Layer;
+
+struct Project {
+    layers: Vec<Layer>,
+    palette: Palette
+}
+
+impl Project {
+    fn new(layers) -> Result<Project, String> {
+        todo!();
+    }
+}
