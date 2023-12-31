1444b5fe5311f40d8af0050547ca3bdf00ced508
Bhavya Kukkar <bhavyakukkar@gmail.com>
2023-12-31 19:44:49 +0530

    Lib-ify crate
    lib.rs
    - Since this crate will be imported into pixylenetui and other
      derivatives, it has been converted into a library crate

---
+++ src/lib.rs
@@ -0,0 +1,11 @@
+#![allow(warnings)]
+extern crate savefile;
+#[macro_use]
+extern crate savefile_derive;
+
+pub mod grammar;
+pub mod elements;
+pub mod project;
+pub mod action;
+pub mod file;
+pub mod pixylene;
