ab902a762a91dd781a44c355b4f6547a4e1245be
Bhavya Kukkar <bhavyakukkar@gmail.com>
2024-01-02 21:21:13 +0530

    Display errors in a single line
    src/grammar.rs
    - newline decoration removed from Decorate::output() to let errors print
      on the same line (the decoration was flawed because indentation would
      require overcomplication of this feature)
    
    src/notes.rs
    - some more ramblings

--- src/grammar.rs
+++ src/grammar.rs
@@ -2,14 +2,15 @@ pub struct Decorate;
 impl Decorate {
     pub fn output(name: String, _type: Option<String>, contents: Option<String>) -> String {
         format!(
-            "{}{} ({})",
+            "{}{}( {} )",
             name,
             match _type {
                 Some(_type) => format!("::{}", _type),
                 None => "".to_string(),
             },
             match contents {
-                Some(contents) => format!("\n  {}\n", contents),
+                //Some(contents) => format!("\n  {}\n", contents),
+                Some(contents) => format!("{}", contents),
                 None => "".to_string(),
             }
         )
