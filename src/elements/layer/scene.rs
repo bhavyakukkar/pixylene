fa6158c3dc603f02df59c4c0054af00b89c0009b
Bhavya Kukkar <bhavyakukkar@gmail.com>
2024-01-04 20:30:54 +0530

    Fix Scene Errors
    - make get_pixel() and set_pixel() error messages more descriptive
    - change type of error returned by new() to static string

--- src/elements/layer/scene.rs
+++ src/elements/layer/scene.rs
@@ -1,4 +1,4 @@
-use crate::elements::common::{Coord, Pixel};
+use crate::elements::common::{ Coord, Pixel };
 
 #[derive(Savefile)]
 pub struct Scene {
@@ -7,19 +7,20 @@ pub struct Scene {
 }
 
 impl Scene {
-    pub fn new(dim: Coord, grid: Vec<Option<Pixel>>) -> Result<Self, String> {
-        if grid.len() == (dim.x*dim.y).try_into().unwrap() {
+    pub fn new(dim: Coord, grid: Vec<Option<Pixel>>) -> Result<Self, &'static str> {
+        if grid.len() as isize == (dim.x*dim.y) {
             Ok(Self{ dim, grid })
         }
         else {
-            Err(format!("Scene dimensions do not match pixel grid"))
+            Err("Scene dimensions do not match pixel grid")
         }
     }
     pub fn get_pixel(&self, coord: Coord) -> Result<Option<Pixel>, String> {
         if coord.x >= 0 && coord.x < self.dim.x && coord.y >= 0 && coord.y < self.dim.y {
             Ok(self.grid[(coord.x*self.dim.y + coord.y) as usize])
         } else {
-            Err(format!("Cannot get pixel {} from scene of dimensions {}", coord, self.dim))
+            Err(format!("Cannot get_pixel from invalid {} on scene of dimensions {}, \
+                         valid values are between {} and {}", coord, self.dim, Coord{ x: 0, y: 0 }, self.dim.add(Coord{ x: -1, y: -1 })))
         }
     }
     pub fn set_pixel(&mut self, coord: Coord, new_pixel: Option<Pixel>) -> Result<(), String> {
@@ -27,7 +28,8 @@ impl Scene {
             self.grid[(coord.x*self.dim.y + coord.y) as usize] = new_pixel;
             Ok(())
         } else {
-            Err(format!("cannot set_pixel at invalid {} on scene of dimensions {}", coord, self.dim))
+            Err(format!("cannot set_pixel at invalid {} on scene of dimensions {}, \
+                         valid values are between {} and {}", coord, self.dim, Coord{ x: 0, y: 0 }, self.dim.add(Coord{ x: -1, y: -1 })))
         }
     }
 }
