64813b9a33b840bac8d69000ed586795d2f61f5b
Bhavya Kukkar <bhavyakukkar@gmail.com>
2024-01-12 15:29:16 +0530

    temp fix to prevent toggling cursor outside scene

--- src/action/actions/toggle_cursor_at_cursor.rs
+++ src/action/actions/toggle_cursor_at_cursor.rs
@@ -13,7 +13,10 @@ impl Action for ToggleCursorAtCursor {
         if let Some(matched_cursor) = project.cursors.iter().position(|&c| c == self.cursor) {
             project.cursors.remove(matched_cursor);
         } else {
-            project.cursors.push(self.cursor);
+            //temporary code that misbehaves when trying outside scene, fix after implemented
+            //Project::add_cursor()
+            project.cursors.push(Cursor{ layer: 0, coord: Coord::zero() });
+            project.set_cursor(project.cursors.len() - 1, self.cursor)?;
         }
         let toggle_cursor_at_cursor = ToggleCursorAtCursor { cursor: self.cursor };
         Ok(vec![Change::StartEnd(Rc::new(RefCell::new(toggle_cursor_at_cursor)))])
