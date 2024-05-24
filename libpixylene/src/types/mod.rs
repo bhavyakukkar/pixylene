3554d36189056a2a7fb5e945bb4829d9f55f0205
Bhavya Kukkar <bhavyakukkar@gmail.com>
2024-05-24 14:47:18 +0530

    include PCoordContainer so can be used from external crates

--- libpixylene/src/types/mod.rs
+++ libpixylene/src/types/mod.rs
@@ -5,7 +5,7 @@ mod ucoord;
 pub use self::ucoord::{ UCoord };
 
 mod pcoord;
-pub use self::pcoord::{ PCoord };
+pub use self::pcoord::{ PCoord, PCoordContainer };
 
 mod pixel;
 pub use self::pixel::{ Pixel, TruePixel, TruePixelError, IndexedPixel };
